a0283736119337688dbfe47810ffe6d1c6caa83b
sun520999 <70926724+sun520999@users.noreply.github.com>
2021-09-26 19:21:43 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -24,12 +24,12 @@ def grab(url):
     response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://raw.githubusercontent.com/sun520999/YouTube_M3U/main/assets/404.m3u')
+            print('https://cdn.jsdelivr.net/gh/sun520999/YouTube_M3U/assets/whhd.mp4')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://raw.githubusercontent.com/sun520999/YouTube_M3U/main/assets/404.m3u')
+            print('https://cdn.jsdelivr.net/gh/sun520999/YouTube_M3U/assets/whhd.mp4')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
